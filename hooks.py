fe7ce3d804c44abb4c128b68b628043f59ca649f
shenweiyan <weiyanshum@gmail.com>
2024-08-08 10:34:23 +0800

    增加 51.LA 站点数据统计, 开启数据挂件功能 v1.0

--- hooks.py
+++ hooks.py
@@ -8,4 +8,4 @@ def on_page_markdown(markdown, *, page, config, files):
 """
 
 def on_config(config, **kwargs):
-    config.copyright = f"版权所有 © 2023-{datetime.now().year} <a href=\"https://github.com/orgs/BioITee/discussions\" target=\"_blank\">BioIT 爱好者</a>"
+    config.copyright = f"版权所有 © 2023-{datetime.now().year} <a href=\"https://github.com/orgs/BioITee/discussions\" target=\"_blank\">BioIT 爱好者</a> | <a href=\"http://beian.miit.gov.cn/\" target=\"_blank\" rel=\"noopener\">粤ICP备16023717号</a>"
